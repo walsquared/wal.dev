f379afa8054f4c48fd703e61c752ee48621263cb
Walsker <walsker@walwal.me>
2020-10-09 11:34:40 -0400

    Remove old Gatbsy project

--- src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Button } from './button'
-export { default as ContactPrompt } from './contactPrompt'
-export { default as Layout } from './layout'
-export { default as SEO } from './seo'
--- src/hooks/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as useIntersectionObserver } from './useIntersectionObserver'
-export { default as useInterval } from './useInterval'
-export { default as useScrollHeight } from './useScrollHeight'
